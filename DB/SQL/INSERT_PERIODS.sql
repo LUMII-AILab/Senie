270ca3184680c5bf38b40547c976b4788b1b7dc4
Normunds Grūzītis <normundsg@gmail.com>
2016-07-10 18:01:09 +0300

    DB/SQL

--- DB/SQL/INSERT_PERIODS.sql
+++ DB/SQL/INSERT_PERIODS.sql
@@ -1,2 +1,4 @@
-INSERT INTO periods (name) VALUES ('17. gadsimts');
-INSERT INTO periods (name) VALUES ('18. gadsimts');
+INSERT INTO periods VALUES (1, '17. gadsimts');
+INSERT INTO periods VALUES (2, '18. gadsimts');
+INSERT INTO periods VALUES (3, '16. gadsimts');
+INSERT INTO periods VALUES (4, '19. gadsimts');
