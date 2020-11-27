1d4533ad7cc917a0c1fcfd067bb5daf64c550993
Lauma <lauma.pretkalnina@gmail.com>
2020-11-27 13:34:31 +0200

    Skripts, kas apkopo @-prefiksotos kodus, lai var uztaisīt par viņiem dokumentāciju

---
+++ Unicoding/LvSenieUtils/CodeCollector.pm
@@ -0,0 +1,92 @@
+package LvSenieUtils::CodeCollector;
+use strict;
+use utf8;
+use warnings;
+use IO::Dir;
+use IO::File;
+use File::Copy;
+
+use Exporter();
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(collect);
+
+sub collect
+{
+	autoflush STDOUT 1;
+	if (@_ < 1)
+	{
+		print <<END;
+Script for collecting all \@-prefixed codes in the input files.
+
+Params:
+   space separated list of input dirs
+
+AILab, LUMII, 2020, provided under GPL
+END
+		exit 1;
+	}
+
+	my @inputDirs = @_;
+	my $codeCollector = {};
+
+	for my $inDirName (@inputDirs)
+	{
+		my $dir = IO::Dir->new($inDirName) or (warn "Folder $inDirName is not available: $!" and next);
+		while (defined(my $inFile = $dir->read))
+		{
+			if ((-f "$inDirName/$inFile") and $inFile =~ /^(.*?)(_Unicode)?(_unhyphened)?\.txt$/)
+			{
+				my $nameStub = $1;
+				$nameStub = "VD1689_94/$nameStub" if ($inDirName =~ /VD(1689_94)?$/);
+				$nameStub = "JT1685/$nameStub" if ($inDirName =~ /JT(1685)?$/);
+				eval
+				{
+					local $SIG{__DIE__} = sub { warn $_[0] }; # This magic makes eval warn on die.
+					$codeCollector = &reviewFile( "$inDirName/$inFile", $nameStub, $codeCollector);
+				};
+			}
+		}
+	}
+
+	&printResult($codeCollector, 'SENIE-codes.txt');
+}
+
+sub reviewFile
+{
+	my $filePath = shift @_;
+	my $fileCode = shift @_;
+	my $codeCollector = shift @_;
+
+	my $in = IO::File->new($filePath, "< :encoding(UTF-8)")
+		or die "Could not open file $fileCode from $filePath: $!";
+
+	while (my $line = <$in>)
+	{
+		while ($line =~ /\@ ?([\w\d]+) ?\{/g)
+		{
+			$codeCollector->{$1}->{$fileCode} = 1;
+		}
+	}
+
+	return $codeCollector;
+}
+
+sub printResult
+{
+	my $codeCollector = shift @_;
+	my $resultFile = shift @_;
+
+	my $out = IO::File->new($resultFile, "> :encoding(UTF-8)")
+		or die "Could not open file $resultFile: $!";
+
+	foreach my $code (sort keys %$codeCollector)
+	{
+		print $out "$code\t";
+		print $out (join ', ', sort keys %{$codeCollector->{$code}});
+		print $out "\n";
+	}
+
+	$out->close;
+}
+
+1;
---
+++ Unicoding/runCodeCollector-sample.bat
@@ -0,0 +1,5 @@
+:: Run for already collected folders like this.
+perl -e "use LvSenieUtils::CodeCollector qw(collect); collect(@ARGV)" data data-VD
+::perl -e "use LvSenieUtils::CodeCollector qw(collect); collect(@ARGV)" data data-JT data-VD
+
+pause
