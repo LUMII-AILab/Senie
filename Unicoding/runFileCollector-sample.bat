d1c6a1addeb1bbb5e4b1e5154645da142ebcdb7b
Lauma <lauma.pretkalnina@gmail.com>
2020-12-03 20:15:25 +0200

    Uztīrīts

--- Unicoding/runFileCollector-sample.bat
+++ Unicoding/runFileCollector-sample.bat
@@ -1,7 +1,7 @@
 :: Run for full corpuss like this. Parameter must give full infix for the files
 :: to be selected - everything between source code (e.g., Baum1699_LVV and
 :: .txt) except leading underscore.
-perl -e "use LvSenieUtils::FileCollector qw(collect); collect(@ARGV)" Unicode
+::perl -e "use LvSenieUtils::FileCollector qw(collect); collect(@ARGV)" Unicode
 ::perl -e "use LvSenieUtils::FileCollector qw(collect); collect(@ARGV)"
 
 pause
