dd1ead045a51ae3f1904d930bf1c5a298d20d65f
Lauma <lauma.pretkalnina@gmail.com>
2021-03-31 15:04:34 +0300

    Apraksts.

--- Unicoding/LvSenie/Vert/IndexTypeCatalog.pm
+++ Unicoding/LvSenie/Vert/IndexTypeCatalog.pm
@@ -1,15 +1,21 @@
 package LvSenie::Vert::IndexTypeCatalog;
 use strict;
 use utf8;
-use warnings FATAL => 'all';
+use warnings;
+
+use IO::File;
 
 use Exporter();
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(getIndexType);
 
+# Path to file where indexing type for each source is listed
 our $sourcePath = '../Sources/indexing.txt';
+# Catalog mapping source code, e.g., Has1550_PN or JT1685/1J to indexing type
+# GNP, GLR, LR, or P.
 our $indexSpec = loadCatalog;
 
+# Load file where indexing type for each source is listed
 sub loadCatalog
 {
 	my $in = IO::File->new($sourcePath, "< :encoding(UTF-8)")
@@ -29,6 +35,9 @@ sub loadCatalog
 	return $result;
 }
 
+# Get indexing code (currently GNP, GLR, LR, or P) from Senie source name.
+# For bible books source name must contain collection code first, then tab or /,
+# and then book code, e.g., JT1685/1J.
 sub getIndexType
 {
 	my $source = join '/', @_;
