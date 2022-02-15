043d82f84c21d26372213ade3029e253fe1af00a
lauma <lauma.pretkalnin@gmail.com>
2022-02-15 19:12:06 +0200

    Vēl viens noderīgs darbināšanas piemērs

--- Unicoding/runFileCollector-sample.bat
+++ Unicoding/runFileCollector-sample.bat
@@ -3,10 +3,12 @@
 :: .txt) except leading underscore.
 
 :: This collect everything in folders data, data-VD, data-JT, data-Apokr.
+::perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)" Unicode_unhyphened
 ::perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)" Unicode
 ::perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)"
 
 :: This collect everything in folder data with appropriate subfolders.
+::perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)" Unicode_unhyphened
 ::perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)" Unicode
 ::perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)"
 
