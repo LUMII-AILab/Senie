ad2b079203b9bce3b8eadb745c31e6d62f7fa273
lauma <lauma.pretkalnin@gmail.com>
2022-03-18 18:44:33 +0200

    Paraugs, kā automātiski atlasīt failus priekš tīmekļa sadaļām inverse un freq

--- Unicoding/runFileCollector-sample.bat
+++ Unicoding/runFileCollector-sample.bat
@@ -1,15 +1,22 @@
-:: Run for full corpuss like this. Parameter must give full infix for the files
-:: to be selected - everything between source code (e.g., Baum1699_LVV and
-:: .txt) except leading underscore.
+REM Run for full corpuss like this. Parameter must give full infix for the files
+REM to be selected - everything between source code (e.g., Baum1699_LVV and
+REM .txt) except leading underscore.
 
-:: This collect everything in folders data, data-VD, data-JT, data-Apokr.
-::perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)" Unicode_unhyphened
-::perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)" Unicode
-::perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)"
+REM This collect everything in folders data, data-VD, data-JT, data-Apokr.
+REM perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)" Unicode_unhyphened
+REM perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)" Unicode
+REM perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)"
 
-:: This collect everything in folder data with appropriate subfolders.
-::perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)" Unicode_unhyphened
-::perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)" Unicode
-::perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)"
+REM This collect everything in folder data with appropriate subfolders.
+REM perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)" Unicode_unhyphened
+REM perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)" Unicode
+REM perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)"
+
+REM This ir for Web/inverse
+REM perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)" inverse
+REM This ir for Web/freq
+REM perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)" frequencies
+REM perl -e "use LvSenie::Utils::FileCollector qw(collectNested); collectNested(@ARGV)" frequencies_lower
+
 
 pause
