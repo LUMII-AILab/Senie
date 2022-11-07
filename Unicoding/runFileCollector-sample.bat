4a2921bc660a7a68a0c51ca61f4776108937e1e0
lauma <lauma.pretkalnin@gmail.com>
2022-11-07 17:43:25 +0200

    Mapju nosaukumi.

--- Unicoding/runFileCollector-sample.bat
+++ Unicoding/runFileCollector-sample.bat
@@ -2,7 +2,7 @@ REM Run for full corpuss like this. Parameter must give full infix for the files
 REM to be selected - everything between source code (e.g., Baum1699_LVV and
 REM .txt) except leading underscore.
 
-REM This collect everything in folders data, data-VD, data-JT, data-Apokr.
+REM This collect everything in folders data, data-VD1689_94, data-JT1685, data-Apokr1689.
 REM perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)" Unicode_unhyphened
 REM perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)" Unicode
 REM perl -e "use LvSenie::Utils::FileCollector qw(collectFlat); collectFlat(@ARGV)"
