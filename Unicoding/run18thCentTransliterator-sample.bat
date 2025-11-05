b39accfc629f1bffefa982d94e7f5c47cbffb633
lauma <lauma.pretkalnina@gmail.com>
2025-11-05 18:04:50 +0200

    Testa faila nosaukums.

--- Unicoding/run18thCentTransliterator-sample.bat
+++ Unicoding/run18thCentTransliterator-sample.bat
@@ -1,6 +1,6 @@
 
 REM Run for single plain text file like this.
-perl -e "use LvSenie::Translit::Transliterator qw(process18thCentFile); process18thCentFile(@ARGV)" text.txt
+perl -e "use LvSenie::Translit::Transliterator qw(process18thCentFile); process18thCentFile(@ARGV)" test.txt
 
 REM Run for single Senie Corpus DSL text file like this.
 ::perl -e "use LvSenie::Translit::Transliterator qw(process18thCentFile); process18thCentFile(@ARGV)" StendGF1774_AGG_Unicode_unhyphened.txt StendGF1774_AGG_Unicode_translitered.txt 1
